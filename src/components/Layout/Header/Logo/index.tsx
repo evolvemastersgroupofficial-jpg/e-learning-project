90519ef82e7e852af47385117fd9198e1123463b
Sama Wael Saad AbdElrasol <samawael@Samas-MacBook-Pro.local>
2025-11-04 13:15:39 +0200

    logo

--- src/components/Layout/Header/Logo/index.tsx
+++ src/components/Layout/Header/Logo/index.tsx
@@ -10,7 +10,7 @@ const Logo: React.FC = () => {
         alt="logo"
         width={160}
         height={50}
-        style={{ width: "25%", height: "25%" }}
+        style={{ width: "20%", height: "20%" }}
         quality={100}
       />
     </Link>
